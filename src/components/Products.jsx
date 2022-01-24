47e7d9004b2a8da0c5cde59536e76a54fe217caf
navneshranjan <navneshranjan@gmail.com>
2022-01-24 22:11:11 +0530

    [cart error resolved]

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from "react";
+// import axios from "axios";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
-import ProductItems from "./ProductItems";
-import { useCart } from "react-use-cart";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useCart } from "react-use-cart";
+import { db } from "../firebase";
 import currencyFormat from "../helper/currencyFormat";
+import ProductItems from "./Product_Items";
 
 const Products = () => {
   const [product, setProduct] = useState([]);
   const { totalItems, cartTotal } = useCart();
-  // Fetch the product....
+
+  // fetch the product here
   useEffect(() => {
     const fetchProduct = async () => {
       // this code was fetching the data from the API I created
       // const { data } = await axios.get("http://localhost:3300/product");
       // setProduct(data.products);
+
       const fetchedProduct = await getDocs(collection(db, "products"));
       fetchedProduct.forEach((doc) => {
         let docItem = {
@@ -30,18 +33,23 @@ const Products = () => {
 
   return (
     <>
+      {/* {product.map((item) => (
+        <ProductItems key={item.id} {...item} item={item} />
+      ))} */}
+
       {product.map((item, id) => (
         <ProductItems key={id} {...item} item={item} />
       ))}
+
       <div className="footer_cart_info">
         <div className="price-component">
           {/* item count */}
           <p className="price_info">
-            {totalItems > 0 ? totalItems : ""}
-            {""}
-            {totalItems < 1 ? "addProduct" : totalItems > 1 ? "items " : "item"}
+            {totalItems > 0 ? totalItems : ""}{" "}
+            {totalItems < 1 ? "Add product" : totalItems > 1 ? "ITEMS" : "ITEM"}
           </p>
-          {currencyFormat(cartTotal)}(plus taxes);
+          {/* total cart value */}
+          {currencyFormat(cartTotal)} (plus taxes)
         </div>
         {totalItems > 0 ? (
           <div className="cart-component">
